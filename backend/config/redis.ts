65d9487d6f0af2d9603a4fae5a978b56bf1597de
Madhav Bansal <madha@MacBook-Air-2.local>
2026-06-24 00:58:38 +0530

    redis url

--- backend/config/redis.ts
+++ backend/config/redis.ts
@@ -1,15 +1,13 @@
 import Redis from 'ioredis'
 
-export const redisOptions = {
-  host : process.env.REDIS_HOST || 'localhost',
-  port : Number(process.env.REDIS_PORT) || 6379
-}
-
-const redis = new Redis(redisOptions)
+const redis = process.env.REDIS_URL 
+  ? new Redis(process.env.REDIS_URL)
+  : new Redis({
+      host: process.env.REDIS_HOST || 'localhost',
+      port: Number(process.env.REDIS_PORT) || 6379
+    });
 
 redis.on('connect', () => console.log('Redis connected'))
 redis.on('error', (err) => console.error('Redis error:', err))
 
 export default redis
-
-
